cf417addefe535977452af84537f9bf5c028b0c7
mazen <94920084+hagguimazen@users.noreply.github.com>
2022-03-20 17:57:17 +0100

    init

---
+++ client/components/TopNav.js
@@ -0,0 +1,94 @@
+import { useState, useEffect, useContext } from "react";
+import { Menu } from "antd";
+import Link from "next/link";
+import {
+  AppstoreOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
+  CoffeeOutlined,
+} from "@ant-design/icons";
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+import SubMenu from "antd/lib/menu/SubMenu";
+
+const { Item } = Menu;
+
+const TopNav = () => {
+  const [current, setCurrent] = useState("");
+
+  const { state, dispatch } = useContext(Context);
+  const { user } = state;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    router.push("/login");
+  };
+
+  return (
+    <Menu mode="horizontal" selectedKeys={[current]}>
+      <Item
+        key="/"
+        onClick={(e) => setCurrent(e.key)}
+        icon={<AppstoreOutlined />}
+      >
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </Item>
+
+      {user === null && (
+        <>
+          <Item
+            key="/login"
+            onClick={(e) => setCurrent(e.key)}
+            icon={<LoginOutlined />}
+          >
+            <Link href="/login">
+              <a>Login</a>
+            </Link>
+          </Item>
+
+          <Item
+            key="/register"
+            onClick={(e) => setCurrent(e.key)}
+            icon={<UserAddOutlined />}
+          >
+            <Link href="/register">
+              <a>Register</a>
+            </Link>
+          </Item>
+        </>
+      )}
+
+      {user !== null && (
+        <SubMenu
+          icon={<CoffeeOutlined />}
+          title={user && user.name}
+          className="float-end"
+        >
+          <Item
+            onClick={logout}
+            icon={<LogoutOutlined />}
+            className="float-end"
+          >
+            Logout
+          </Item>
+        </SubMenu>
+      )}
+    </Menu>
+  );
+};
+
+export default TopNav;
